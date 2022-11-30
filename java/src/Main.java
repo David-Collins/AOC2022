22b1b79a5ae6a4269e06b265e9845b4e3debe5f6
david.collins <david.collins@cvent.com>
2022-11-30 11:41:02 -0500

    Initial commit

---
+++ java/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {}
+    public static List<String> readInputLines(int day) {
+        try{
+            File text = new File(System.getProperty("user.dir") + "/input/day" + day +".txt");
+            Scanner sc = new Scanner(text);
+            int acc = 0;
+            List<String> lines = new ArrayList<>();
+            while(sc.hasNextLine()) {
+                String currLine = sc.nextLine();
+                lines.add(currLine);
+            }
+            return lines;
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    public static List<String> readInputLines(String day) {
+        try{
+            File text = new File(System.getProperty("user.dir") + "/input/day" + day +".txt");
+            Scanner sc = new Scanner(text);
+            int acc = 0;
+            List<String> lines = new ArrayList<>();
+            while(sc.hasNextLine()) {
+                String currLine = sc.nextLine();
+                lines.add(currLine);
+            }
+            return lines;
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
