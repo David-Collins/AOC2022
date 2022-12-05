a2bf57006077bfd247093f6a8cda51a0f2a39a80
david.collins <david.collins@cvent.com>
2022-12-05 11:17:03 -0500

    Day 5 solution

---
+++ java/src/Day5.java
@@ -0,0 +1,75 @@
+import java.io.CharArrayReader;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day5 {
+    public static void main(String[] args){
+        System.out.println("Part 1:");
+        part1();
+        System.out.println("Part 2:");
+        part2();
+    }
+    public static void part1() {
+        List<String> lines = Main.readInputLines("5-stack");
+        List<Stack<Character>> crates = new ArrayList<>();
+        for (String line : lines) {
+            Stack<Character> stack = new Stack<>();
+            for (char c : line.toCharArray()) {
+                stack.push(c);
+            }
+            crates.add(stack);
+        }
+        lines = Main.readInputLines("5");
+        Pattern reg = Pattern.compile("move\\s([\\d]+)\\sfrom\\s([\\d]+)\\sto\\s([\\d]+)");
+        Map<Integer, Map<Integer, Integer>> coords = new HashMap<>();
+        for(String line : lines) {
+            Matcher m = reg.matcher(line);
+            m.find();
+            int count = Integer.parseInt(m.group(1));
+            Stack<Character> src = crates.get(Integer.parseInt(m.group(2)) - 1);
+            Stack<Character> dest = crates.get(Integer.parseInt(m.group(3)) - 1);
+            for (int i = 0; i < count; i++) {
+                dest.push(src.pop());
+            }
+        }
+        String result = "";
+        for (Stack<Character> crate: crates) {
+            result += crate.peek();
+        }
+        System.out.println(result);
+    }
+    public static void part2() {
+        List<String> lines = Main.readInputLines("5-stack");
+        List<Stack<Character>> crates = new ArrayList<>();
+        for (String line : lines) {
+            Stack<Character> stack = new Stack<>();
+            for (char c : line.toCharArray()) {
+                stack.push(c);
+            }
+            crates.add(stack);
+        }
+        lines = Main.readInputLines("5");
+        Pattern reg = Pattern.compile("move\\s([\\d]+)\\sfrom\\s([\\d]+)\\sto\\s([\\d]+)");
+        Map<Integer, Map<Integer, Integer>> coords = new HashMap<>();
+        for(String line : lines) {
+            Matcher m = reg.matcher(line);
+            m.find();
+            int count = Integer.parseInt(m.group(1));
+            Stack<Character> src = crates.get(Integer.parseInt(m.group(2)) - 1);
+            Stack<Character> dest = crates.get(Integer.parseInt(m.group(3)) - 1);
+            Stack<Character> temp = new Stack<>();
+            for (int i = 0; i < count; i++) {
+                temp.push(src.pop());
+            }
+            for (int i = 0; i < count; i++) {
+                dest.push(temp.pop());
+            }
+        }
+        String result = "";
+        for (Stack<Character> crate: crates) {
+            result += crate.peek();
+        }
+        System.out.println(result);
+    }
+}
